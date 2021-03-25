15500ca670aba3d510c8f8fd66900292baf05932
Ruowen Tang <ruowen.tang@gmail.com>
2021-03-25 19:30:38 -0400

    Updated HTML layout

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,257 +1,75 @@
-// /*
-//  * Client-side JS logic goes here
-//  * jQuery is already loaded
-//  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
-//  */
+const createTweetElement = function(data) {
 
-// $(document).ready(() => {
+  const $article = $('<article>');
 
-//   $('section#tweet-list').on("mouseenter", ".tweets", function(e) {
+  const $div = $('<div>', {
+    class: 'header'
+  }).appendTo($article);
 
-//       $(this).find('.icons').addClass('visible');
+  $('<img>', {
+    attr: { 
+      src: data.user.avatars, 
+      alt: 'avatar' }
+  }).appendTo($div);
 
-//   });
+  $('<h4>', {
+    text: data.user.name
+  }).appendTo($div);
 
-//   $('section#tweet-list').on("mouseleave", ".tweets", function(e) {
-      
-//           $(this).find('.icons').removeClass('visible');
-      
-//   });
+  $('<span>', {
+    text: data.user.handle
+  }).appendTo($div);
 
-//   function createTweetElement ( data ) {
+  const $p = $('<p>', {
+    text: data.content.text
+  }).appendTo($article);
 
-//     const $article = $('<article>',{
-//       class: 'tweets'
-//     }).data('id', data._id);
-    
-//     const $header = $('<header>',{
-//       class: 'clearfix'
-//     }).appendTo($article);
-      
-//     $('<img>', {
-//       attr: { 
-//         src: data.user.avatars.small, 
-//         alt: 'avatar' }
-//     }).appendTo($header);
-
-//     $('<h2>', {
-//       text: data.user.name
-//     }).appendTo($header);
-
-//     $('<span>', {
-//       text: data.user.handle
-//     }).appendTo($header);
-
-//     const $main = $('<main>').appendTo($article);
-
-//     $('<p>',{
-//       text: data.content.text
-//     }).appendTo($main);
-
-//     // const $footer = $('<footer>', {
-//     //   text: moment(moment(data.created_at).format()).fromNow()
-//     // }).appendTo($article);
-    
-//     const $div = $('<div>',{
-//       class: 'icons' 
-//     }).appendTo($footer);
-      
-//     $('<i>', {
-//       class: 'fa fa-flag',
-//       attr: { 'aria-hidden': 'true' }
-//     }).appendTo($div);
-      
-//     $('<i>', {
-//       class: 'fa fa-retweet',
-//       attr: { 'aria-hidden': 'true' }
-//     }).appendTo($div);
-    
-//     $('<i>',{
-//       class: ( data.likes === 0 ) ? "fa fa-heart-o" : "fa fa-heart",
-//       attr: { 'aria-hidden': 'true' }
-//     }).appendTo($div);
-
-
-//       return $article;
-//   }
-
-//   function renderTweets(tweets) {
-//       // loops through tweets
-//         // calls createTweetElement for each tweet
-//         // takes return value and appends it to the tweets container
-    
-//         for (let tweet of tweets) {
-//             const $tweet = createTweetElement(tweet);
-//             $('#tweet-list').prepend($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-//         }
-    
-//   }
-
-//   function loadTweets() {
-
-//     $.ajax({
-//       url:'/tweets',
-//       method: 'get',
-//       dataType: 'json'
-//     })
-//     .done(function(result){
-//       renderTweets(result);
-//     })
-//     .fail(function(error, xhr) {
-//       console.log(error, xhr);
-//     });
-//   }
-
-//   function validateForm(form) {
-//     const textarea = $(form).find('textarea');
-//     const counter = Number($(form).find('span.counter').text());
-//     const empty = textarea.val() === "";
-//     const tooLong = counter < 0;
-
-//     const errorMessages = { 
-//       errors: empty || tooLong || false,
-//       empty:     (empty) ? "Please, you must enter a tweet!" : false,
-//       tooLong:   (tooLong) ? "Please, make sure your tweet is less than 140 characters!" : false
-//     }
-
-    
-//     return(errorMessages);
-
-//   }
-
-//   function printErrorMessages(errors) {
-
-//     const ul = $('ul.errors');
-//     ul.remove();
-
-//     const $ul = $('<ul>').addClass('errors');
-//     $ul.css('color', 'red');
-//     const main = $('main.container');
-
-//     delete errors.errors;
-
-//     for (let errorMsg in errors) {
-//       if (errors[errorMsg]) {
-//         const $li = $('<li>').text(errors[errorMsg]);
-//         $ul.append($li);
-//       }
-//     }
-
-//     main.prepend($ul);
-
-//   }
-
+  const $footer = $('<footer>');
   
-//   function resetForm(form) {
-//     form.trigger('reset');
-//     form.parent().find('span.counter').text(140);
-//   }
-
-//   $('section.new-tweet input[type=submit]').on('click', (e) => {
-
-//     e.preventDefault();
-//     const form = $(e.target).parent();
-//     const errorMessages = validateForm(form);
-
-//     if (errorMessages.errors) {
-//       printErrorMessages(errorMessages);
-//     } else {
-//       $.ajax({
-//           url: '/tweets',
-//           method: 'post',
-//           data: form.serialize()
-//           }
-//         )
-//         .done(function(data){
-//           console.log("done");
-//           renderTweets([data]);
-//           resetForm(form);
-//         })
-//         .fail(function(error, xhr) {
-//           console.log(error, xhr); 
-//         })
-            
-//       }
-
-//     });
+  $('<div>', {
+    class: 'date',
+    text: Date(data.created_at * 1000).substr(0, 25)
+  }).appendTo($footer);
+    
+  $('<div>', {
+    class: 'icons' 
+  }).appendTo($footer);
+      
+    // $('<i>', {
+    //   class: 'fa fa-flag',
+    //   attr: { 'aria-hidden': 'true' }
+    // }).appendTo($div);
+      
+    // $('<i>', {
+    //   class: 'fa fa-retweet',
+    //   attr: { 'aria-hidden': 'true' }
+    // }).appendTo($div);
+    
+    // $('<i>',{
+    //   class: ( data.likes === 0 ) ? "fa fa-heart-o" : "fa fa-heart",
+    //   attr: { 'aria-hidden': 'true' }
+    // }).appendTo($div);
 
 
-//     $('#nav-bar a').on('click', function(e) {
-//       const newTweetContainer = $('section.new-tweet');
-//       const textarea = newTweetContainer.find('textarea');
-//       newTweetContainer.slideToggle('slow', function(){
-//         textarea.focus();
-//       });
-//     })
+  return $article;
 
-//     function updateLike(target, attValue) {
+}
 
-//       $(target).data("likes", attValue);
-//       if ( attValue === 0 ) {
-//         $(target).removeClass('fa fa-heart');
-//         $(target).addClass('fa fa-heart-o');
-//       } else {
-//         $(target).removeClass('fa fa-heart-o');
-//         $(target).addClass('fa fa-heart');
-//       }
-//     }
+// Test / driver code (temporary). Eventually will get this from the server.
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+}
 
+const $tweet = createTweetElement(tweetData);
 
-//     $('section#tweet-list').on('click', '[class*="fa-heart"]', function( e ){
-
-//       const id = $(this).closest('article').data('id'); 
-//       console.log({id});
-
-//       const data = {
-//         likes:  $(this).data('likes')
-//       }
-
-//       $.ajax({
-//         url: `/tweets/${id}`,
-//         type: 'post',
-//         headers: { 'X-HTTP-Method-Override': 'PUT' },
-//         data: data
-//       })
-//       .done(function(like){
-        
-//         updateLike(e.target, like);
-                
-//       })
-//       .fail(function(error, xhr){
-//         console.log(error, xhr);
-//       })
-
-//     });
-
-//     $('form.new-user').on('submit', function(e) {
-
-//       e.preventDefault();
-
-//       const username  = $(this).find('input[name="username"]').val();
-//       const email     = $(this).find('input[name="email"]').val();
-//       const password  = $(this).find('input[name="password"]').val();
-
-//       user = {
-//         username,
-//         email,
-//         password
-//       };
-
-//       $.ajax({
-//         url:  '/users',
-//         type: 'post',
-//         data: user,
-//       })
-//       .done(function(response){
-//         console.log(response);
-//       })
-//       .fail(function(error, xhr){
-//         console.log(error, xhr);
-//       })
-
-//     });
-
-//     loadTweets();
-
-
-//  })  
+// Test / driver code (temporary)
+console.log($tweet); // to see what it looks like
+$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
